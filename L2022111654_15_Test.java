bd30051c6d8424c3d7d0d95a86a661580e8c93ca
polar <polarc5625@163.com>
2024-11-20 01:35:20 +0800

    add test ,delete useless code

--- L2022111654_15_Test.java
+++ L2022111654_15_Test.java
@@ -1,14 +1,14 @@
 import org.junit.jupiter.api.Test;
 import static org.junit.Assert.assertEquals;
-import Solution15;
+//import Solution15;
 
 class L2022111654_15_Test {
     @Test
     void testCompareVersion() {
-        Solution solution = new Solution();
+        Solution15 solution = new Solution15();
 
         // 示例测试用例
-        assertEquals(0, Solution15.compareVersion("1.01", "1.001"));
+        assertEquals(0, solution.compareVersion("1.01", "1.001"));
         assertEquals(0, solution.compareVersion("1.0", "1.0.0"));
         assertEquals(-1, solution.compareVersion("0.1", "1.1"));
 
